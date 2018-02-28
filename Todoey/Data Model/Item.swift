8acd07ebec60a8be261f9d5153a562d6d6d48f38
Abdul Khan <khalik@abduls-imac.local>
2018-02-28 12:25:19 +0530

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,22 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Abdul Khalik Khan on 28/02/18.
+//  Copyright © 2018 Haadi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
+
+
+
+
+
+
